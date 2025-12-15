a4f7d960c97c036a23df46dd6390fd816c309653
Amine Badraoui <aminebadraoui94@gmail.com>
2025-12-14 19:00:35 -0500

    add cors rules

--- backend/main.py
+++ backend/main.py
@@ -72,7 +72,14 @@ app = FastAPI(lifespan=lifespan)
 # CORS middleware
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173", "http://127.0.0.1:5173", "https://www.youtube.com"],
+    allow_origins=[
+        "http://localhost:5173",
+        "http://127.0.0.1:5173",
+        "https://www.youtube.com",
+        "https://www.clipcoba.com",
+        "https://www.dev.clipcoba.com"
+    ],
+    allow_origin_regex=r"https://.*\.clipcoba\.com",
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
